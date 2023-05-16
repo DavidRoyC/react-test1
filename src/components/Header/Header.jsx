0539a6b560c8be3695f732f6be47d151a98ade91
David Roy <david.roy.casao@gmail.com>
2023-05-16 20:10:38 +0200

    Estilo Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,23 @@ import './Header.css';
 const Header = ({ subtitle, setTheme }) => {
   let theme = useContext(ThemeContext);
 
+  const toggleTheme = () => {
+    let change = {
+      darkmode: !theme.darkmode,
+      icon: theme.darkmode ? 'dark_mode' : 'light_mode'
+    }
+    setTheme(change);
+  }
+
   return (
     <header className="App-header">
-          <h1>Bride's target list</h1>
-          <h2>{subtitle}</h2>
+      <div>
+        <h1>Bride's target list</h1>
+        <h2>{subtitle}</h2>
+      </div>
+      <div className="options">
+        <button type="button" onClick={toggleTheme}><span className="mi-outlined">{theme.icon}</span></button>
+      </div>  
     </header>
   )
 }
