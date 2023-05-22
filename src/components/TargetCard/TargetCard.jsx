723b169754be7e715697aca7e107e1db278d0fe7
David Roy <david.roy.casao@gmail.com>
2023-05-22 15:05:08 +0200

    Dia/noche Listado

--- src/components/TargetCard/TargetCard.jsx
+++ src/components/TargetCard/TargetCard.jsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+
+import ThemeContext from "../../contexts/ThemeContext";
 
 import "./TargetCard.css";
 
 const TargetCard = ({target}) => {
+  let theme = useContext(ThemeContext);
 
-  return (<li className={'Target ' + target.state}>
+  return (<li className={`Target ${target.state} ${theme.darkmode? 'dark' : 'light'}`}>
             <div className="action"><button onClick={() => {}}><span className="mi-outlined">edit</span></button></div>
             <div className="action"><button onClick={() => {}}><span className="mi-outlined">delete</span></button></div>
             <div><Link to={`/target/${target.id}`} className="name">{target.name}</Link></div>
--- src/components/TargetsList/TargetsList.jsx
+++ src/components/TargetsList/TargetsList.jsx
@@ -1,17 +1,20 @@
 import { useState, useContext } from "react";
+
 import PublicTargets from "../../contexts/PublicTargets";
+import ThemeContext from "../../contexts/ThemeContext";
 
 import "./TargetsList.css";
 import TargetCard from "../TargetCard/TargetCard";
 import SearchBar from "../SearchBar/SearchBar";
 
 const TargetsList = () => {
+  let theme = useContext(ThemeContext);
   const {targets, loadingTargets, errorTargets} = useContext(PublicTargets);
   const [filter, setFilter] = useState('');
 
   return (
     <div>
-      <ul className="targetsList">
+      <ul className={`targetsList ${theme.darkmode? 'dark' : 'light'}`}>
         {
           errorTargets && <div>Error cargando objetivos</div>
           // <ErrorCard at={'Objetivos'} />
@@ -26,6 +29,14 @@ const TargetsList = () => {
             .filter(target => target.name.toLowerCase().includes(filter.toLowerCase()))
             .map((target, index) => <TargetCard target={target} key={index} />)
         }
+        <li className={'Target'}>
+            <div className="action">
+              <button onClick={() => {}} className="newTarget">
+                <span className="mi-outlined">add</span>
+                <span className="name">Añadir nuevo objetivo</span>
+              </button>
+            </div>
+          </li>
       </ul>
       <SearchBar dataFilter={{filter, setFilter}} />
     </div>
