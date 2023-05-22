a7770a0cb13fb050449fd2e5a0a4cab2ba1c6bc3
David Roy <david.roy.casao@gmail.com>
2023-05-22 13:50:47 +0200

    Fix warnings

--- src/components/TopNavbar/TopNavbar.jsx
+++ src/components/TopNavbar/TopNavbar.jsx
@@ -8,14 +8,6 @@ import './TopNavbar.css';
 const TopNavbar = ({ subtitle, setTheme }) => {
   let theme = useContext(ThemeContext);
 
-  const toggleTheme = () => {
-    let change = {
-      darkmode: !theme.darkmode,
-      icon: theme.darkmode ? 'dark_mode' : 'light_mode'
-    }
-    setTheme(change);
-  }
-
   return (
     <nav className={`topNavbar ${theme.darkmode? 'dark' : 'light'}`}>
       <Link to="/" className="nav-item">Inicio</Link>
